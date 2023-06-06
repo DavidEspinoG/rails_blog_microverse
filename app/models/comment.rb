51e14f36bdf9de92cd2c0b1cd712aa6c1370dd92
David Espino <davidespinog@gmail.com>
2023-06-06 10:22:09 -0600

    Add update_comment_counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,13 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_comment_counter
+    post = Post.find(self.post_id)
+    if !post.comments_counter
+      post.update(comments_counter: 1)
+    else
+      post.update(comments_counter: post.comments_counter + 1)
+    end
+  end
 end
