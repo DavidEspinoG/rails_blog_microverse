373970cb3b65bc59add65b25ec61cbda2be4c242
David Espino <davidespinog@gmail.com>
2023-06-06 10:37:01 -0600

    Fix rubocop offenses

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,13 +2,12 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_likes_counter 
+  def update_likes_counter
     post = Post.find(post_id)
-    if !post.likes_counter 
-      post.update(likes_counter: 1)
-    else 
+    if post.likes_counter
       post.update(likes_counter: post.likes_counter + 1)
+    else
+      post.update(likes_counter: 1)
     end
   end
-
 end
