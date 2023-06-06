2fe735bef57059468b63dd1bb46d64608d908dd1
David Espino <davidespinog@gmail.com>
2023-06-06 09:48:41 -0600

    Add update_post_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def update_post_counter
+    @author = User.find(author_id)
+    if @author.posts_counter
+      @author.posts_counter += 1
+    else
+      @author.posts_counter = 1
+    end
+    @author.save
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
-
+  # attr_accessor :post_counter
   def last_three_posts
-    self.posts.limit(3).order(created_at: :desc)
+    posts.limit(3).order(created_at: :desc)
   end
-
 end
