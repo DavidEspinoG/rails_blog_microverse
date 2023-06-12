0cc4fa1a31c148e71cfa7c062cfec5f6299eca33
David Espino <davidespinog@gmail.com>
2023-06-12 15:28:58 -0600

    Add test for show method in users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,17 +2,27 @@ require 'rails_helper'
 
 RSpec.describe 'Users' do
   describe 'GET /index' do
+    before { get '/'}
     it 'has an status 200' do
-      get '/'
       expect(response.status).to eq(200)
     end
-    it "shows the static text" do 
-      get '/'
+    it "shows the static placeholder text" do 
       expect(body).to match('<h1>List all users</h1>')
     end
-    it 'shows the correct view' do 
-      get '/'
+    it 'shows the correct template' do 
       expect(response).to render_template('list')
+    end
+  end
+  describe 'GET /show' do
+    before { get '/users/10'}
+    it 'has a 200 status' do 
+      expect(response.status).to eq(200) 
+    end
+    it 'renders the correct template' do 
+      expect(response).to render_template('show')
+    end
+    it 'shows the correct placeholder text' do 
+      expect(body).to match('<h2>Info about user with id: 10</h2>')
     end
   end
 end
