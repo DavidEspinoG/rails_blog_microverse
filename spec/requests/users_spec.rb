2252272cf371dc6d5b69c70119112779fc6695fe
David Espino <davidespinog@gmail.com>
2023-06-12 14:53:48 -0600

    Add test using capybara for user

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
   describe 'GET /index' do
+    it 'has an status 200' do
+      get '/'
+      expect(response.status).to eq(200)
+    end
     it "shows the static text" do 
       visit 'http://localhost:3000'
       expect(page).to have_content('List all users')
     end
+    it 'should render the correct view' do 
+      get '/'
+      response.should render_template('list')
+    end
   end
 end
+
+
