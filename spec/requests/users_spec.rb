b338771014e697507767e5c0c5c3bf87cc49a990
David Espino <davidespinog@gmail.com>
2023-06-12 15:16:46 -0600

    Refactor user test for not using capybara

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
+RSpec.describe 'Users' do
   describe 'GET /index' do
     it 'has an status 200' do
       get '/'
       expect(response.status).to eq(200)
     end
     it "shows the static text" do 
-      visit 'http://localhost:3000'
-      expect(page).to have_content('List all users')
-    end
-    it 'should render the correct view' do 
       get '/'
-      response.should render_template('list')
+      expect(body).to match('<h1>List all users</h1>')
+    end
+    it 'shows the correct view' do 
+      get '/'
+      expect(response).to render_template('list')
     end
   end
 end
