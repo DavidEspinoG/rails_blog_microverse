28c496864e5d03a3118cb66f8798bc9bbffdadaa
David Espino <davidespinog@gmail.com>
2023-06-13 15:45:48 -0600

    Add create new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,22 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:user_id])
+    @post = Post.find(params[:post_id])
   end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new author_id: current_user.id, title: params[:title], 
+    text: params[:text], comments_counter: 0, likes_counter: 0 
+    if @post.save
+      redirect_to "/users/#{@post.author.id}/posts/#{@post.id}"
+    else 
+      render :new, status: :unprocessable_entity
+      puts @post.errors.full_messages_for(:title).class
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "users#index"
   get 'users/:id', to: 'users#show'
   get 'users/:id/posts', to: 'posts#index'
   get 'users/:user_id/posts/:post_id', to: 'posts#show'
+  get 'posts/new', to: 'posts#new'
+  post 'posts/new', to: 'posts#create'
+  root "users#index"
+
 end
