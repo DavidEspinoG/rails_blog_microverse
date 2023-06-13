2cc053d76b52b2faec2d79c7100b7e2375b6da77
David Espino <davidespinog@gmail.com>
2023-06-12 19:01:55 -0600

    Update posts index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @id = params[:id]
+    @user = User.find(params[:id])
+    @posts = @user.posts
+    @comments = Post.first.five_most_recent_comments
   end
 
   def show
