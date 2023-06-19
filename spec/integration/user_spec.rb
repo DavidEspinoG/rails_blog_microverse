81706c9447505cdc22ac5eeca941830bc1cdc597
David Espino <davidespinog@gmail.com>
2023-06-19 12:57:03 -0600

    Add integration test

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'User integration spec', type: :system do
   describe 'User index' do
     let!(:user_one) { User.create name: 'Jose', posts_counter: 0 }
     let!(:user_two) { User.create name: 'Carlos', posts_counter: 0 }
-    let!(:post) { Post.new author: user_two }
+    let!(:post) { Post.create author_id: user_two.id, comments_counter: 0, likes_counter: 0, title: 'title' }
 
     it 'Should render users names' do
       visit '/'
@@ -16,6 +16,13 @@ RSpec.describe 'User integration spec', type: :system do
       visit '/'
       expect(page).to have_content('Number of posts: 0')
       expect(page).to have_content('Number of posts: 1')
+    end
+    
+   
+    it 'should redirect to users/:id view' do
+      visit '/'
+      click_link href: "/users/#{user_one.id}"
+      expect(page).to have_current_path("/users/#{user_one.id}") 
     end
   end
 end
