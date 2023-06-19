c3e8652414625b45619fd76847a1c52641b08aaa
David Espino <davidespinog@gmail.com>
2023-06-19 15:04:33 -0600

    Add tests for posts#index

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'post#index', type: :system do 
   let!(:user_two) { User.create name: 'Carlos', posts_counter: 0 }
   let!(:post) { Post.create author_id: user_two.id, comments_counter: 0, likes_counter: 0, title: 'title', text: 'something' }
+  let!(:comment) {Comment.create author: user_two, post: post, text: 'hello'}
 
   it 'should render the author profile picture' do
     visit "users/#{user_two.id}/posts"
@@ -29,4 +30,16 @@ RSpec.describe 'post#index', type: :system do
   it 'should render the firsts comments' do 
     visit "users/#{user_two.id}/posts"
   end
+
+  it 'should render the number of comments' do 
+    visit "users/#{user_two.id}/posts"
+    element = find_by_id("comments_post_#{post.id}")
+    expect(element).to have_content("Number of comments: #{post.comments_counter}")
+  end
+  it 'should render the number of likes' do 
+    visit "users/#{user_two.id}/posts"
+    element = find_by_id("likes_post_#{post.id}")
+    expect(element).to have_content("Number of likes: #{post.likes_counter}")
+  end
+
 end
