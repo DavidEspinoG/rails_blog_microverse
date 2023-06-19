534cdd7f36f6d60bfe960d841f0233d4cf5f460b
David Espino <davidespinog@gmail.com>
2023-06-19 15:28:23 -0600

    Add integration tests for post#index

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'post#index', type: :system do 
   let!(:user_two) { User.create name: 'Carlos', posts_counter: 0 }
+  let!(:user_three) { User.create name: 'Santiago', posts_counter: 0 }
   let!(:post) { Post.create author_id: user_two.id, comments_counter: 0, likes_counter: 0, title: 'title', text: 'something' }
-  let!(:comment) {Comment.create author: user_two, post: post, text: 'hello'}
+  let!(:post_two) { Post.create author_id: user_three.id, comments_counter: 0, likes_counter: 0, title: 'title', text: 'something' }
+  let!(:comment) {Comment.create author: user_two, post: post_two, text: 'hello'}
+  let!(:comment_two) {Comment.create author: user_two, post: post_two, text: 'hello'}
+  let!(:comment_three) {Comment.create author: user_two, post: post_two, text: 'hello'}
+  let!(:comment_four) {Comment.create author: user_two, post: post_two, text: 'hello'}
+  let!(:comment_five) {Comment.create author: user_two, post: post_two, text: 'hello'}
+  let!(:comment_six) {Comment.create author: user_two, post: post_two, text: 'hello'}
 
   it 'should render the author profile picture' do
     visit "users/#{user_two.id}/posts"
@@ -40,6 +47,19 @@ RSpec.describe 'post#index', type: :system do
     visit "users/#{user_two.id}/posts"
     element = find_by_id("likes_post_#{post.id}")
     expect(element).to have_content("Number of likes: #{post.likes_counter}")
+  end
+
+  it 'should render pagination if the post has more than 5 comments' do
+    visit "users/#{user_three.id}/posts"
+    expect(page).to have_content('Comment pagination')
+  end
+
+  context 'post detail link' do 
+    it 'should redirect to that post detail' do 
+      visit "users/#{user_two.id}/posts"
+      click_link href: "/users/#{user_two.id}/posts/#{post.id}"
+      expect(page).to have_current_path("/users/#{user_two.id}/posts/#{post.id}")
+    end
   end
 
 end
