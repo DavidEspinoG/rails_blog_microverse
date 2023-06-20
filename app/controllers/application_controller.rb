268f8b1c9ca89c1c3c25fd7b27e3da864cb38026
David Espino <davidespinog@gmail.com>
2023-06-20 15:38:52 -0600

    Update new user view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :update_allowed_parameters, if: :devise_controller?
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {
+      |u| u.permit(:posts_counter, :name, :email, :password)
+    }
+  end
 end
