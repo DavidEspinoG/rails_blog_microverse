1a34c9a5963d4079d3c414e653f0e7a6174cba49
David Espino <davidespinog@gmail.com>
2023-06-21 16:43:26 -0600

    Fix migratioin error

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_191706) do
     t.integer "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -62,7 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_191706) do
     t.string "unconfirmed_email"
     t.string "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
