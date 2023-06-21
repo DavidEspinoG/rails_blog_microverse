641d9225ad23b44cfe2bb386c6a8832acbeea756
David Espino <davidespinog@gmail.com>
2023-06-20 20:39:30 -0600

    add navigation buttons

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  # get 'users/:id', to: 'users#show'
-  # get 'users/:id/posts', to: 'posts#index'
-  # get 'users/:user_id/posts/:post_id', to: 'posts#show'
+  get 'users/:id', to: 'users#show'
+  get 'users/:id/posts', to: 'posts#index'
+  get 'users/:user_id/posts/:post_id', to: 'posts#show'
   get 'posts/new', to: 'posts#new'
   post 'posts/new', to: 'posts#create'
   get 'comment/new/:post_id', to: 'comment#new' 
